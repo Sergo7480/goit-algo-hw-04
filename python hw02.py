4f6f5a9f9ee4b90e07f4721156088fd1271f0fcb
Sergo7480 <157243352+Sergo7480@users.noreply.github.com>
2025-01-21 18:27:33 +0200

    Update python hw02.py

--- python hw02.py
+++ python hw02.py
@@ -1,21 +1,19 @@
 def get_cats_info(path):
     try:
-        cats_list = []
-        with open(path, 'r', encoding='utf-8') as file:
+        cats_info = []
+    
+        with open(path, 'r', encoding="utf-8") as file:
             for line in file:
-                cat_id, cat_name, cat_age = line.strip().split(',')
-                cat_info = {
-                    "id": cat_id,
-                    "name": cat_name,
-                    "age": cat_age
-                }
-                cats_list.append(cat_info)
-        return cats_list
+                id, name, age = line.strip().split(',')
+                cats_dict = {"id": id, "name": name, "age": age}
+                cats_info.append(cats_dict)
+
+        return cats_info
     except FileNotFoundError:
-        print(f"Файл '{path}' не знайдено.")
-        return []
+        print(f"Файл '{path}' не знайдено")
+        return None, None
     except ValueError:
         print(f"Помилка при обробці файлу '{path}'. Перевірте формат даних.")
-        return []
-cats_info = get_cats_info("path/to/cats_file.txt")
+        return None, None
+cats_info = get_cats_info("C:/Users/Серёжа/git/first_nepo/cats_file.txt")
 print(cats_info)
